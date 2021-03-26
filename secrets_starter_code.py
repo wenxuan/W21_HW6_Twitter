b5dd0ddfd5b01e85614f202ef7b48f59fbed6999
wenxuan <wenxuan@umich.edu>
2021-03-26 00:18:22 -0400

    added more starter code files

---
+++ secrets_starter_code.py
@@ -0,0 +1,8 @@
+#fill this in with your credentials and rename to secrets.py
+#make sure to gitignore this file! you will lose points if you share this in your github!
+
+TWITTER_API_KEY=""
+TWITTER_API_SECRET=""
+TWITTER_ACCESS_TOKEN=""
+TWITTER_ACCESS_TOKEN_SECRET=""
+
